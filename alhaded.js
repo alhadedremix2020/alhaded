e28226cda9016883e488da8b03021911f9d9f654
alhadedremix2020 <74561285+alhadedremix2020@users.noreply.github.com>
2020-11-16 20:33:48 +0200

    Create alhaded.js

---
+++ alhaded.js
@@ -0,0 +1,40 @@
+$(".ubnr").remove();
+$(`<link rel="stylesheet" href="https://rawcdn.githack.com/alhadedremix2020/alhaded/cedb2791da008be352560d7ff338ebe66c6afc76/alhaded.css"></style>`).insertBefore("body");
+$('.hand.nosel.fl.uzr.border').css({'margin': '1px 0',borderRight: '1px solid #EDEDED',borderLeft: '1px solid #adadad',borderTop: '1px solid #EDEDED',borderBottom: '1px solid #EDEDED',paddingTop: '3px',borderRadius: '5px'});
+
+
+
+$(`<link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet">
+<marquee direction="right" width="65%" " id="jfa-w" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif; height: 20px;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">  |آهلا وسهلا بكم في شات عشقتك حياك هنا نورتنا ·● مع تمنياتنا للجميع بأطيب الاوقات  </marquee></font></b></td>
+`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');
+$('.room.borderg.hand.nosel.fl').css({'margin': '1px 0',borderRight: '3px solid #EDEDED',borderLeft: '3px solid #EDEDED',borderTop: '1px solid #EDEDED',borderBottom: '1px solid #EDEDED',paddingTop: '3px',borderRadius: '5px'});
+$('.fr.borderg a').text(' عمر الحديد')
+
+$( "a.label.label-primary.fl > img.fl" ).replaceWith('<img src="" class="fl" style="margin-right: 0px; width: 0px;">');
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img id="as13" src="https://www.sudfachat.com/sico/1580237645774.png" width="70%" style="">`)
+
+//$(`${'<img tabindex="0" class="al120 fl " style="margin-left: -20px; padding:6px; width:42px;margin-top: -2px;" src="https://b.top4top.io/p_1749833q11.gif" data-original-title="" title="">'}`)['insertAfter']('img.fl.nosel.emobox');
+$('div#tlogins button.btn.btn-primary')['click'](function () {
+    var b = setInterval(function () {
+        var a = $('.pmsgc')['length'];
+        0 < a ? ($(`${'<iframe src="https://raw.githack.com/alhadedremix2020/alhaded/main/Radio-chatnsh.html" width="23px" height="23px" scrolling="no" frameborder="no" marginheight="0" marginwidth="0" style="float: left;width: 23px;margin-top: 2.4px;height: 23px;position: absolute;left: 65px;border-radius: 100%;"></iframe>'}`)['insertAfter']('button.fa.fa-sign-out.fl.btn.btn-primary'), clearInterval(b)) : console['log'](a)
+    }, 1)
+}), $('div#tlogins button.btn.btn-primary')['click'](function () {
+    setTimeout(function () {
+        refreshonline()
+    }, 0)
+});
+$(`${'<img tabindex="0" class="smoke fl " style="padding:5px;width: 55px;margin-top: -6px;margin-left: -17px;margin-right: -8px;"data-original-title="" title="">'}`)['insertAfter']('img.fl.nosel.emobox')
+$(`<center><div style="position: static;width: 100%;" onclick="setTimeout(function(){fixSize();},800);"><div width="99.5%" id="design-a"style="background-color: white;color: #d9534f;border-bottom: 2px solid #f0ad4e;border-top: 0px solid #6b5f5f;padding-bottom: 2px;border-radius: 0px 0px 15px 15px;border-bottom-style: dashed;" ><font id="sultan" class=" fahd-jfa-w.net"><u><b>قـوأنـيـن ألشـات</b></u> </font><div id="spanan" style="font-family: jazeera-light, FontAwesome;color: red;text-align: right;display:none;" ><p style="text-align: center"><b><font color="#000080"> قـوأنـيـن شات عشقتك للجوال </font></b></p></p><div style="float: right;padding-right: 20px;">- 1</div> يمنع افتعال وفتح الحوارات الطائفية والمذهبية والمناطقية والعرقية والقبلية وغيرها من الحوارات العنصرية .
+</p><div style="float: right;padding-right: 20px;">- 2</div>تنبية ل الزائر في حالة واجهت مشكلة من قبل احد السوابر يرجى التواصل مع الادارة .
+
+</p><div style="float: right;padding-right: 20px;">- 3</div> يمنع منعا باتا استخدام الايحاءات والالفاظ المخلة بالادب والشريعه الاسلامية وجرح مشاعر الاخرين
+  </p><div style="float: right;padding-right: 20px;">- 4</div> التزام الادب مع اخوانك في الدردشة بنات او شباب .
+ </p><div style="float: right;padding-right: 20px;">- 5</div> يمنع منعا باتا نشر او الترويج ل اي موقع اخر كي لا تتعرض لحذف العضوية والحظر
+ </p><div style="float: right;padding-right: 20px;">- 6</div> مراقبة الله في النشر على الحائط وتذكر “ مراقبة الله لك ”
+ </p><div style="float: right;padding-right: 20px;">- 7</div> يمنع استخدام خاصية التبية ل ازعاج الاخرين بكلام غير لائق.
+ </p><div style="float: right;padding-right: 20px;">- 8</div> يمنع نشر الارقام في العام
+ </p><div style="float: right;padding-right: 20px;">- 9</div> عزيزي الزائر اذا قام احد المتواجدين بمضايقتك فقط قم بتصوير "سكرين شوت" لكلامه ثم التوجه الى للإداره او أي سوبر موجود داخل الدردشة لكي يتم اجراء اللازم معه
+ </p><div style="float: right;padding-right: 20px;">- 10</div> (https)بنضام تشفير العالمي (ip) نحن نحمي عنوان الـ 
+ </p><div style="float: right;padding-right: 20px;">- 11</div> عزيزي الزائر فضلا وليس امرا يرجى اتباع قواعد وشروط الموقع كي لا تعرض نفسك للطرد او الحظر كلنا اخوة في الموقع ونتشرف بوجودكم معنا .
+</b><p style="text-align: center"><u><b><a target="_blank" href="https://t.me/ihtc0"> وللتواصل مع الاداره {للحالات الطارئه} اضغط هنا </a></b></u></font></div> </div></div><center>`).insertBefore('#d2');$("#design-a").click(function(){$("#spanan").slideToggle(function(){});});
